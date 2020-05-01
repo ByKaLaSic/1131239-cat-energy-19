773d3e9d82c9506d8d785e8c967802d37bbe47de
Максим <makszavr.1234554321@yandex.ru>
2020-05-01 16:18:22 +0300

    Проверка другого варианта формы

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,11 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var filter = document.querySelector('.filter__form');
+
+if (filter) {
+  var requireds = filter.querySelectorAll('input[required]');
+}
+
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -12,3 +18,15 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+var addTheNecessaryHandler = function (required) {
+  if (filter) {
+    required.addEventListener('input invalid', function () {
+      required.classList.add('.filter__input-invalid');
+    });
+  }
+};
+
+for (var i = 0; i < requireds.length; i++) {
+  addTheNecessaryHandler(requireds[i]);
+};
