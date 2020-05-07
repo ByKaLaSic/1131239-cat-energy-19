66448c6497e2f460074048bf283da9ce4f4ddb04
Максим <makszavr.1234554321@yandex.ru>
2020-05-07 19:44:36 +0300

    Изменил обработчик качества и поставил cover

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task("images", function () {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({ optimizationLevel: 3 }),
-      imagemin.mozjpeg({ quality: 75, progressive: true }),
+      imagemin.mozjpeg({ quality: 90, progressive: true }),
       imagemin.svgo()
     ]))
     .pipe(gulp.dest("build/img"));
